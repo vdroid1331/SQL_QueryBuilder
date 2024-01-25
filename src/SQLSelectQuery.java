4ac66148a3f101210a41684253d56d8e05caee24
vdroid1331 <65962958+vdroid1331@users.noreply.github.com>
2024-01-25 18:52:30 +0530

    Added a SQL Select query interface

---
+++ src/SQLSelectQuery.java
@@ -0,0 +1,29 @@
+public interface SQLSelectQuery {
+    SQLSelectQuery select(String fields);
+    SQLSelectQuery select(String[] fields);
+    SQLSelectQuery from(String fields);
+
+    SQLSelectQuery where(String condition);
+
+    SQLSelectQuery orderBy(String condition);
+    SQLSelectQuery orderBy(String condition, boolean orderByAscending);
+    SQLSelectQuery orderBy(String condition, String orderByModifier);
+    SQLSelectQuery groupBy(String fields);
+    SQLSelectQuery groupBy(String[] fields);
+
+
+    // for usage without the from method
+    SQLSelectQuery fromAndJoin(String joinTableOne, String joinTableTwo, String firstTableField, String secondTableField);
+
+    // to be used with the from method
+    SQLSelectQuery joinOn(String joinTableTwo, String firstTableField, String secondTableField);
+
+
+
+    String build();
+
+
+
+
+
+}
