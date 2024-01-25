dc0e9b3fb0af97de967645f522826e9c4f9533b7
vdroid1331 <65962958+vdroid1331@users.noreply.github.com>
2024-01-25 20:19:23 +0530

    Added a basic Select Query Class using the builder pattern.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//        GenericSelectQuery SelectQuery = (GenericSelectQuery) new GenericSelectQuery().select(new String[]{"user.name", "courses.title"}).from("user").where("age > 10").orderBy("name").groupBy(new String[]{"name", "age"}).joinOn("courses", "id", "user_id");
+        GenericSelectQuery SelectQuery = (GenericSelectQuery) new GenericSelectQuery()
+                .select(new String[]{"user.name", "courses.title"})
+                .from("user")
+                .where("age > 10")
+                .orderBy("name")
+                .groupBy("name, age")
+                .joinOn("courses", "id", "user_id");
+
+        System.out.println(SelectQuery.build());
+
+
     }
 }
